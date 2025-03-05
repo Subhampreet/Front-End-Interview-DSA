34727a3c1f417984ee5cb6ef8e54ba0c4276cb42
Subhampreet Mohanty <subhampreet18@gmail.com>
2025-03-05 19:41:29 +0530

    React Concept : React Memo Code Snippet

---
+++ BigFrontEnd/React JS Concept Snippets/react.memo.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const Counter = ({ count }) => {
+  console.log("Counter Re-rendered");
+  return <h1>Count : {count}</h1>;
+};
+
+// const MemoizedCounter = React.memo(Counter);
+
+const ReactMemo = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      {/* <Counter count={count} /> */}
+      <MemoizedCounter count={count} />
+      <button onClick={() => setCount(count + 1)}>
+        Click to Increase Count
+      </button>
+
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default ReactMemo;
